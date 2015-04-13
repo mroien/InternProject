ba02a7d33a4cdcb91c8e9e9ae93a3a2eae037d7e
unknown <toien@MWC-TOien.MSC.local>
2015-04-13 17:28:01 -0500

    Best Buy is done and working with position: absolute and z-index

--- Websites/javascript.js
+++ Websites/javascript.js
@@ -1,1 +1,22 @@
-Tim Oien Intern Project JavaScript
+//  Tim Oien Intern Project JavaScript
+// Best Buy DONE
+$(document).ready(function() {
+		jQuery("#price .cart-button").after('<a href="Save on Samsung -- $299.99 at bestbuy.com - TrackIf.html"><input id="tif-button" type="button" /><div id="panel"><img src="samsung.PNG" rel="#watch" class="display-Image"/></a></div>');
+		jQuery("#tif-button, #panel").mouseenter(function() {
+			$("#panel").stop().show("fold", 1000);
+		}).mouseleave(function() {
+			$('#panel').stop().slideUp();
+		});
+	});
+
+
+
+/*
+array of objects
+check domain
+add button function
+button overlay
+[amazon.com, #price .cart-button]
+jQuery('link[rel="canonical"]').attr('href').match(/^https?:\/\/[^\/]*?(bestbuy)\.com/)[1];
+
+*/
